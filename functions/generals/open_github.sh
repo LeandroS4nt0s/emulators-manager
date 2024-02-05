cb86ad1a726f92f552c6f9089c914330611a4cc0
Leandro Santos <jose.martins@accountfy.com>
2024-02-05 01:03:08 -0300

    changing github link

--- functions/generals/open_github.sh
+++ functions/generals/open_github.sh
@@ -2,5 +2,5 @@
 
 # Function to open repository link in the browser
 open_github_repository() {
-    open https://github.com/LeandroS4nt0s/simulators-manager
+    open https://github.com/LeandroS4nt0s/emulators-manager
 }
